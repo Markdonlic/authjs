7ea46c42cbd4a3245cd93e53622bd53b11b6318f
MPC <markwilliam01259@gmail.com>
2023-06-20 13:41:22 +0100

    update

--- app/admin/dashboard/messages/page.jsx
+++ app/admin/dashboard/messages/page.jsx
@@ -7,8 +7,8 @@ const ContactUsers = () => {
   return (
     <div className="w-full px-6 flex flex-col gap-4 justify-center items-center">
       <h1 className="font-semibold text-lg capitalize">contact admin</h1>
-      <div className="w-full rounded-2xl text-xs bg-neutral-800 px-4 py-4">
-        <div className="md:col-span-2 rounded-2xl text-xs shadow-xl dark:shadow-none dark:bg-neutral-800 px-4 py-4">
+      <div className="md:col-span-2 rounded-2xl text-xs shadow-xl dark:shadow-none dark:bg-neutral-800 px-4 py-4">
+        <div className="flex justify-between border-b-[1px] border-gray-500 pb-2">
           <p className="text-gray-100 text-justify text-xs">Recent Messages</p>
           <Link href="/" className="text-sky-600">
             show all
